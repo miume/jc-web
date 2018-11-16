5b9475ab19eb335b5fb70841169855be4c65e758
么么 <1538276681@qq.com>
2018-11-16 17:49:39 +0800

    server

--- src/components/roleManagement/permissionManagement.js
+++ src/components/roleManagement/permissionManagement.js
@@ -106,15 +106,138 @@ class PermissionManagement extends React.Component {
             {id:5, name : '基础信息', prefix:'AUTH_', parent:-1},
             {id:6, name : '菜单管理', prefix:'AUTH_MENU_', parent:4},
         ]
-
+        const menu = [
+            {
+              "menuId": 4,
+              "menuName": "用户和权限",
+              "prefix": "AUTH",
+              "menuType": 1,
+              "parent": -1,
+              "menuDTOList": [
+                {
+                  "menuId": 1,
+                  "menuName": "权限管理",
+                  "prefix": "AUTH_AUTH",
+                  "menuType": 2,
+                  "parent": 4,
+                  "menuDTOList": []
+                },
+                {
+                  "menuId": 2,
+                  "menuName": "菜单管理",
+                  "prefix": "AUTH_MENU",
+                  "menuType": 2,
+                  "parent": 4,
+                  "menuDTOList": []
+                },
+                {
+                  "menuId": 3,
+                  "menuName": "角色管理",
+                  "prefix": "AUTH_ROLE",
+                  "menuType": 2,
+                  "parent": 4,
+                  "menuDTOList": []
+                },
+                {
+                  "menuId": 5,
+                  "menuName": "用户管理",
+                  "prefix": "AUTH_USER",
+                  "menuType": 2,
+                  "parent": 4,
+                  "menuDTOList": []
+                },
+                {
+                  "menuId": 6,
+                  "menuName": "部门管理",
+                  "prefix": "AUTH_DEPARTMENT",
+                  "menuType": 2,
+                  "parent": 4,
+                  "menuDTOList": []
+                }
+              ]
+            },
+            {
+              "menuId": 7,
+              "menuName": "质量和流程",
+              "prefix": "PROCESS",
+              "menuType": 1,
+              "parent": -1,
+              "menuDTOList": [
+                {
+                  "menuId": 8,
+                  "menuName": "数据录入",
+                  "prefix": "PROCESS_DATA_IN",
+                  "menuType": 2,
+                  "parent": 7,
+                  "menuDTOList": []
+                },
+                {
+                  "menuId": 9,
+                  "menuName": "流程管理",
+                  "prefix": "PROCESS_PROCESS",
+                  "menuType": 2,
+                  "parent": 7,
+                  "menuDTOList": []
+                },
+                {
+                  "menuId": 10,
+                  "menuName": "待办事项",
+                  "prefix": "PROCESS_TASK",
+                  "menuType": 2,
+                  "parent": 7,
+                  "menuDTOList": []
+                },
+                {
+                  "menuId": 11,
+                  "menuName": "基础数据",
+                  "prefix": "PROCESS_BASE_DATA",
+                  "menuType": 2,
+                  "parent": 7,
+                  "menuDTOList": []
+                }
+              ]
+            },
+            {
+              "menuId": 16,
+              "menuName": "dumin",
+              "prefix": "",
+              "menuType": 1,
+              "parent": -1,
+              "menuDTOList": []
+            }
+          ]
         return (
             <span>
                 <a onClick={this.showModal} value={this.state.value}>权限管理</a>
                 <Modal title='编辑权限' visible={this.state.visible} 
                 onOk={this.handleOk} onCancel={this.handleCancel}
                 okText='确定' cancelText='取消' width='650px' destroyOnClose='true' >
-                <div style={{height:'400px'}}>
-                    <table className="tableHead">
+                <div style={{height:'500px'}}>
+                    <div className='tableHead'>
+                        <span>子模块选择</span>
+                        <span>操作</span>
+                    </div>
+                    <div>
+                    {
+                        this.state.allMenus.map(m1=>{
+                            return (
+                                <div key={m1.menuId}>
+                                    <div className='divborder'><span>{m1.menuName}</span><span></span></div>
+                                        <div>
+                                        {
+                                        m1.menuList.map(m2=>{
+                                            return (
+                                                <div key={m2.menuId} className='divborder'><span>{m2.menuName}</span><span></span></div>
+                                            );
+                                        })
+                                        }
+                                        </div>
+                                </div>
+                            );
+                        })
+                    }
+                    </div>
+                    {/* <table className="tableHead">
                         <colgroup style={{width:'29.5%'}}></colgroup>
                         <colgroup style={{width:'63%'}}></colgroup>
                         <thead>
@@ -133,54 +256,14 @@ class PermissionManagement extends React.Component {
                             this.state.allMenus.map(m1 => 
                                 <tr key={m1.menuId}>
                                     <td style={{textAlign:'left',paddingLeft:'40px'}} ><Icon type="caret-down" theme="filled" />{m1.menuName}</td>
-                                    <td></td>
+                                    <td></td><td></td>
                                 </tr>
-                                // {
-                                // m1.menuDTOList.map(m2=>{
-                                //     <tr key={m2.menuId}>
-                                //         <td><Icon type="caret-right" theme="filled" />{m2.menuName}</td>
-                                //         <td>
-                                //         {
-                                //             api.map(op=>{
-                                //                 <span style={{paddingRight:'12px'}} key={op.id}>
-                                //                 <Checkbox  value={op.name} id={m2.menuId.toString()} onChange={this.change}>{op.name}</Checkbox>
-                                //                 </span>
-                                //             })   
-                                //         }
-                                //         </td>
-                                //     </tr>  
-                                // })
-                                // }
                             )
-                                // mennus.map(v => {
-                                //     return v.parent === -1 ? (
-                                //         <tr key={v.id}>
-                                //             <td style={{textAlign:'left',paddingLeft:'40px'}} ><Icon type="caret-down" theme="filled" />{v.name}</td>
-                                //             <td></td>
-                                //         </tr>
-                                //     ):(
-                                //         <tr key={v.id+'menu'}>
-                                //             <td><Icon type="caret-right" theme="filled" />{v.name}</td>
-                                //             <td>
-                                //                 {
-                                //                     api.map(v1 => {
-                                //                         return (
-                                //                             <span style={{paddingRight:'12px'}} key={v1.id} >  
-                                //                             <Checkbox value={v1.id} id={v.id.toString()} onChange={this.change}>{v1.name}</Checkbox>
-                                //                              {/* <span>{v1.name}</span> <input type='checkbox' value={v1.id} id={v.id} onChange={this.change} /> */}
-                                //                             </span>             
-                                //                         );
-                                //                     })
-                                //                 }
-                                //             </td>
-                                //         </tr>
-                                //     );
-                                // })
                             }
                             </tbody>
-                        </table>
+                        </table> */}
                     </div>
-                </div>
+               
                 </Modal>
             </span>
         );
