2a25782830b07596301f764c0f7fec2a0010f44d
Accumulations <317217599@qq.com>
2018-11-26 23:59:43 +0800

    合格与不合格按钮点击

--- src/components/intermediateProductTest/checkSpanModal.js
+++ src/components/intermediateProductTest/checkSpanModal.js
@@ -3,6 +3,7 @@ import { Input,Button,Table,Radio,Divider } from 'antd';
 import './aePopModal.css';
 import '../Home/page.css';
 import IsQualified from "./isQualified";
+import CheckQualifiedModal from './checkQualifiedModal';
 
 
 const topData = {
@@ -22,13 +23,15 @@ const examineData = {
 //判断类型，如果为新增,则data为空
 //如果为详情和编辑，则通过id查询该条数据
 class CheckSpanModal extends React.Component {
-    state = {
-        topData : topData,      //表头数据
-        testData: testData,   // 检验人数据
-        examineData: examineData,  //审核人数据
-        status : '', //0不合格，1合格
-
-    };
+    constructor(props){
+        super(props);
+        this.state = {
+            topData : topData,      //表头数据
+            testData: testData,   // 检验人数据
+            examineData: examineData,  //审核人数据
+            status : '', //0不合格，1合格
+        };
+    }
     columns = [{
         title: '序号',
         dataIndex: 'index',
@@ -71,8 +74,6 @@ class CheckSpanModal extends React.Component {
                 }),
             };
         });
-        const RadioButton = Radio.Button;
-        const RadioGroup = Radio.Group;
         return(
             <div >
                 <div style={{paddingBottom:'55px'}}>
@@ -109,55 +110,22 @@ class CheckSpanModal extends React.Component {
                         scroll={{ y: 250 }}
                     />
                 </div>
-                <div>
-                    可不可以通过调用组件，通过状态来返回不同值
-                    <div id='passDiv' style={{border:'3px solid #999999',width:'130px',float:'right'}}>
-                        <span id='passSpan' onClick={this.selectQualified(1)} style={{display:'block',textAlign:'center',height:'55px',border:'1px solid #999999',color:'#999999',margin:'2px',fontSize:'25px',paddingTop:'8px'}}>合格</span>
-                    </div>
-                    <div id='noPassDiv' style={{border:'3px solid #999999',width:'130px',float:'right'}}>
-                        <span id='noPassSpan' onClick={this.selectQualified(0)} style={{display:'block',textAlign:'center',height:'55px',border:'1px solid #999999',color:'#999999',margin:'2px',fontSize:'25px',paddingTop:'8px'}}>不合格</span>
-                    </div>
-                </div>
+                <CheckQualifiedModal
+                />
+                {/*<div>*/}
+                    {/*/!*可不可以通过调用组件，通过状态来返回不同值*!/*/}
+                    {/*<div id='passDiv' style={{border:'3px solid #999999',width:'130px',float:'right'}}>*/}
+                        {/*<span id='passSpan' onClick={this.selectQualified(1)} style={{display:'block',textAlign:'center',height:'55px',border:'1px solid #999999',color:'#999999',margin:'2px',fontSize:'25px',paddingTop:'8px'}}>合格</span>*/}
+                    {/*</div>*/}
+                    {/*<div id='noPassDiv' style={{border:'3px solid #999999',width:'130px',float:'right'}}>*/}
+                        {/*<span id='noPassSpan' onClick={this.selectQualified(0)} style={{display:'block',textAlign:'center',height:'55px',border:'1px solid #999999',color:'#999999',margin:'2px',fontSize:'25px',paddingTop:'8px'}}>不合格</span>*/}
+                    {/*</div>*/}
+                {/*</div>*/}
             </div>
         )
     }
-    change = (e) => {
-        console.log('eeeee:',e)
-        e.className = 'redClass';
-    }
     /**实现选择合格:1与不合格:0功能 */
-    selectQualified = (status) => {
-        // if(status===0){
-        //     var idPassDiv = document.getElementById('passDiv');
-        //     idPassDiv.style.border = '3px solid #FF3B30';
-        //     var idPassSpan = document.getElementById('passSpan');
-        //     idPassSpan.style.border = '3px solid #FF3B30';
-        //     idPassSpan.style.color = '#FF3B30';
-        // };
-        // if(status===1){
-        //     var idNoPassDiv = document.getElementById('noPassDiv');
-        //     idNoPassDiv.style.border = '3px solid #4BD863';
-        //     var idNoPassSpan = document.getElementById('noPassSpan');
-        //     idNoPassSpan.style.border = '3px solid #4BD863';
-        //     idNoPassSpan.style.color = '#4BD863';
-        // };
-        // switch (status) {
-        //     case 0:
-        //         var idPassDiv = document.getElementById('passDiv');
-        //         idPassDiv.style.border = '3px solid #FF3B30';
-        //         var idPassSpan = document.getElementById('passSpan');
-        //         idPassSpan.style.border = '3px solid #FF3B30';
-        //         idPassSpan.style.color = '#FF3B30';
-        //         break;
-        //     case 1:
-        //         var idNoPassDiv = document.getElementById('noPassDiv');
-        //         idNoPassDiv.style.border = '3px solid #4BD863';
-        //         var idNoPassSpan = document.getElementById('noPassSpan');
-        //         idNoPassSpan.style.border = '3px solid #4BD863';
-        //         idNoPassSpan.style.color = '#4BD863';
-        //         break;
-        // }
-    };
+
     /**---------------------- */
     /**实现字段搜索功能 */
     /**---------------------- */
