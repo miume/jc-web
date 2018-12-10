3a0776054c776add7508cac99d57fd24ffb1a853
miume <1538276681@qq.com>
2018-12-10 15:12:32 +0800

    todolist

--- src/components/todolist/todoprocessed.js
+++ src/components/todolist/todoprocessed.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import {Avatar} from 'antd';
-import {Part} from './part';
+import Part from './part';
 import './todolist.css';
 class TodoProcessed extends React.Component{
     server
@@ -21,7 +20,9 @@ class TodoProcessed extends React.Component{
             createPersonId:-1
         }
         this.fetch = this.fetch.bind(this);
+        this.moveLeft = this.moveLeft.bind(this);
     }
+    /**根据当前登陆用户id获取待办事项 */
     fetch(){
         const id = localStorage.getItem('menuList')?JSON.parse(localStorage.getItem('menuList')).userId:-1;
         axios.get(`${this.server}/jc/common/toDoList/${id}`,{
@@ -30,12 +31,15 @@ class TodoProcessed extends React.Component{
             }
         }).then((data)=>{
             const res = data.data.data;
-            console.log(res[0].details);
             this.setState({
                 createPersonId:res?res[0].commonBatchNumber.createPersonId:-1,
                 data:res[0].details
             })
         })
+    }
+    moveLeft(){
+        const moveItem = document.getElementsByClassName('item2Scroll');
+        
     }
     render(){
         this.server = localStorage.getItem('remote');
@@ -53,16 +57,23 @@ class TodoProcessed extends React.Component{
                          </span>
                      </div>
                     <div className='demo'>
-                        <div className='item item1'><i className='fa fa-2x fa-caret-left'></i></div>
+                        <div className='item item1' onClick={this.moveLeft}><i className='fa fa-2x fa-caret-left'></i></div>
                         <div className='item item2'>
-                            <div className='part'>
+                            <div className='item2Scroll'>
+                            {
+                                this.state.data.map(e=>
+                                    <Part key={e.id}/>
+                                )
+                            }
+                            </div>
+                            {/* <div className='part'>
                                 <span style={{padding:'0 10px'}}><Avatar style={{backgroundColor:'#0079fe'}}><span style={{fontWeight:'bolder'}}>1</span></Avatar></span>
                                 <div>
                                     <p className='partSpan'>有您进行</p>
                                     <p className='partSpan1'>核对基本信息</p>
                                 </div>
                                 <span style={{width:'100px',height:'1.5px', padding:'5px',borderBottom:'1px solid grey'}}></span>
-                            </div>
+                            </div> */}
                         </div>
                         <div className='item item3'><i className='fa fa-2x fa-caret-right'></i></div>
                         <div className='item item4'></div>
