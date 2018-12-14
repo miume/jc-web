287e6bd0fa7453428e4ad408c3db81ad6a88e793
Accumulations <317217599@qq.com>
2018-12-14 15:35:58 +0800

    完善

--- src/components/purchaseCheckReport/purchaseCheckReport.js
+++ src/components/purchaseCheckReport/purchaseCheckReport.js
@@ -13,14 +13,14 @@ class PurchaseCheckReport extends React.Component {
         return(
             <div>
                 <BlockQuote name="进货检测报告" menu="质量与流程" menu2="数据录入"></BlockQuote>
-                <Tabs defaultActiveKey="2" type="card" onChange={this.callback}  style={{padding:'15px'}}>
-                    <TabPane tab="生成" key="1" >
+                <Tabs defaultActiveKey="2"  onChange={this.callback}  style={{padding:'15px'}}>
+                    <TabPane tab={<span style={{paddingLeft:'25px',paddingRight:'25px'}}><i className="fa fa-cube" aria-hidden="true"></i> &nbsp;生成</span>} key="1" >
                         <Pack />
                     </TabPane>
-                    <TabPane tab="审核" key="2" >
+                    <TabPane tab={<span style={{paddingLeft:'25px',paddingRight:'25px'}}><i className="fa fa-certificate" aria-hidden="true"></i> &nbsp;审核</span>} key="2" >
                         <Check />
                     </TabPane>
-                    <TabPane tab="发布" key="3" >
+                    <TabPane tab={<span style={{paddingLeft:'25px',paddingRight:'25px'}}><i className="fa fa-bullhorn" aria-hidden="true"></i> &nbsp;发布</span>} key="3" >
                         <Release />
                     </TabPane>
                 </Tabs>
