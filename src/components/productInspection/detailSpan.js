4199dee73e77c6dc1dbde81e0d1b45079833b782
Accumulations <317217599@qq.com>
2019-01-21 09:45:47 +0800

    成品

--- src/components/productInspection/detailSpan.js
+++ src/components/productInspection/detailSpan.js
@@ -39,7 +39,7 @@ class DetailSpan extends React.Component {
         this.getDetailData = this.getDetailData.bind(this);
     }
 
-    handleCancel = (e) => {
+    handleCancel = () => {
         this.setState({
             visible: false,
         });
