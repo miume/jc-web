9754e05916baa40c91de216f664189d78a505adc
miume <1538276681@qq.com>
2019-03-28 15:17:15 +0800

    权限更新

---
+++ src/components/fns.js
@@ -0,0 +1,9 @@
+const home = {
+    /**用来判断该菜单有哪些操作权限 */
+   judgeOperation(operation,operationCode){
+       var flag = operation?operation.filter(e=>e.operationCode===operationCode):[];
+       return flag.length>0?true:false
+   }
+}
+
+export default home;
