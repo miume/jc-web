e38ee7f42affc24e4d82b1f270e95ae1a9cf06a8
QGQYY <2970663121@qq.com>
2019-08-08 12:50:49 +0800

    批次追溯

---
+++ src/components/batchSearch/searchpart.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {Button, Icon, Input} from 'antd';
+import "./batchSearch.css"
+
+
+export default class SearchPart extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            InputValue:'',
+        }
+    }
+    handleInputChange=(e)=>{
+        this.setState({InputValue:e.target.value})
+        console.log(e.target.value)
+    }
+    render(){
+        return(
+            <span className="batchsearchPart">
+                <Input
+                    id="batchInput"
+                    placeholder="请输入关键字"
+                    value={this.state.InputValue}
+                    onChange={this.handleInputChange}
+                />
+                <Button
+                    type="primary"
+                    style={{marginLeft:10}}
+                    onClick={this.handleSearch}
+                    className='button'
+                >
+                    <Icon type="search" />搜索</Button>
+                <Button
+                    type="primary"
+                    onClick={this.getFetch}
+                    className='button'
+                >
+                    <i className="fa fa-repeat" aria-hidden="true"/> 重置</Button>
+            </span>
+        );
+    }
+    handleSearch=()=>{
+        var params={};
+            params={
+                condition:this.state.InputValue,
+            }
+        //this.props.getTableData(params)
+        //this.props.handleSearch()
+        console.log(params)
+    }
+    getFetch = () => {
+        /**重置时清除搜索框的值 */
+        this.setState({
+            InputValue:'',
+        })
+        //this.props.handleSearch()
+        //this.props.getTableData()
+    }
+}
+
