40c29187fe65da143e6f6844a130c2db1a54038b
Accumulations <317217599@qq.com>
2019-09-18 23:31:52 +0800

    bug

--- src/components/checkPlan/planSwitch.js
+++ src/components/checkPlan/planSwitch.js
@@ -1,5 +1,6 @@
 import React from "react"
 import {Table, Switch, message} from 'antd'
+import axios from "axios";
 
 class PlanSwitch extends React.Component {
     constructor(props) {
@@ -7,10 +8,28 @@ class PlanSwitch extends React.Component {
 
     }
     effFlagChange = (e) => {
-        console.log(e)
-        console.log(this.props.record.code)
-        console.log(this.props.deptCode)
-        console.log(this.props.deviceName)
+        axios({
+            url: `${this.props.url.SpotcheckPlan.update}`,
+            method: 'put',
+            headers: {
+                'Authorization': this.props.url.Authorization
+            },
+            params: {
+                planId:this.props.record.code
+            },
+        }).then((data) => {
+            if (data.data.code===0) {
+                this.props.getTableData({
+                    deptId: this.props.deptCode,
+                    deviceName: this.props.deviceName,
+                })
+                message.info('修改成功')
+            } else {
+                message.info('修改失败,请刷新页面')
+            }
+        }).catch(() => {
+            message.info('修改失败,请刷新页面')
+        });
 
     }
 
@@ -18,7 +37,7 @@ class PlanSwitch extends React.Component {
         return (
             <div>
                 <Switch checkedChildren="开" unCheckedChildren="关"
-                        defaultChecked={this.props.record.effFlag ? false : true}
+                        defaultChecked={this.props.record.effFlag?false:true}
                         onChange={this.effFlagChange}
                 />
             </div>
