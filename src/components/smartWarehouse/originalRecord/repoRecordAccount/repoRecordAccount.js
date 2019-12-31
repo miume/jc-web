d5231f67109f864c414224439c97241f4c4e3feb
Accumulations <317217599@qq.com>
2019-12-31 14:42:48 +0800

    流水

---
+++ src/components/smartWarehouse/originalRecord/repoRecordAccount/repoRecordAccount.js
@@ -0,0 +1,191 @@
+import React from 'react';
+import BlockQuote from "../../../BlockQuote/blockquote";
+import {Spin, message, Table, Tabs, Popconfirm} from "antd";
+import SearchCell from '../../../BlockQuote/newSearchSell';
+import axios from 'axios';
+import InAccount from './inAccount'
+import OutAccount from './outAccount'
+import {getOperations,judgeOperation} from "../../../commom/getOperations";
+
+var data1 = []
+var data2 = []
+for (var i = 0; i < 20; i++) {
+    data1.push({
+        col1: i+1,
+        col2: `物料编码${i+1000}`,
+        col3: '物料名称',
+        col4: '物料批号',
+        col5: '物料类型',
+        col6: `物料小类`,
+        col7: '入库车间',
+        col8: '供应商',
+        col9: i*10+i,
+        col10: (i+1)%5,
+        col11: '合格',
+        col12: '2019年11月11日',
+        col13: 'admin',
+    })
+    data2.push({
+        col1: i+1,
+        col2: `物料编码${i+1000}`,
+        col3: '物料名称',
+        col4: '物料批号',
+        col5: '物料类型',
+        col6: `物料小类`,
+        col7: '供应商',
+        col8: i*10+i,
+        col9: (i+1)%5,
+        col10: '2019年11月11日',
+    })
+}
+
+
+const { TabPane } = Tabs;
+class RepoRecordAccount extends React.Component {
+
+    componentDidMount = () => {
+        this.getTableParams(undefined,"1");
+    }
+    componentWillUnmount = () => {
+        this.setState(() => {
+            return;
+        })
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            searchContent: '',
+            dataSource:[],
+            tabKey:'1'
+        };
+        this.operations = [];
+        this.pagination = {
+            pageSize: 10,
+            current: 1,
+            showSizeChanger: true,//是否可以改变 pageSize
+            showTotal: (total) => `共${total}条记录`,//显示共几条记录
+            pageSizeOptions: ["10", "20", "50", "100"]
+        };
+    }
+
+    render() {
+        this.current = JSON.parse(localStorage.getItem('dataEntry'));
+        this.url = JSON.parse(localStorage.getItem('url'));
+        return (
+            <div>
+                <BlockQuote name={this.current.menuName} menu={this.current.menuParent} menu2='返回' returnDataEntry={this.back}/>
+                <Spin spinning={this.state.loading} wrapperClassName='rightDiv-content'>
+                    <SearchCell flag={true} searchEvent={this.searchEvent} reset={this.reset} placeholder={'物料编码'}/>
+                    <div className='clear'></div>
+                    <Tabs defaultActiveKey='1' onChange={this.tabChange}>
+                        <TabPane tab='入库台帐' key='1'>
+                            <InAccount pagination={this.pagination} dataSource={this.state.dataSource} handleTableChange={this.handleTableChange}/>
+                        </TabPane>
+                        <TabPane tab='出库台帐' key='2'>
+                            <OutAccount pagination={this.pagination} dataSource={this.state.dataSource} handleTableChange={this.handleTableChange}/>
+                        </TabPane>
+                    </Tabs>
+                </Spin>
+            </div>
+        );
+    }
+
+
+    tabChange = (key) => {
+        this.setState({
+            searchContent: undefined,
+            tabKey:key
+        });
+        this.pagination.current = 1;
+        this.getTableParams('',key)
+
+    }
+
+    /**确定获取表格数据的参数*/
+    getTableParams = (value,key) => {
+        let {searchContent} = this.state, {pageSize,current} = this.pagination,
+            params = {
+                condition: value === undefined ? searchContent : value,
+                size: pageSize,
+                page: current
+            };
+        this.getTableData(params,key);
+    }
+
+    /**获取表格数据*/
+    getTableData = (params,key) => {
+        if (key==="1"){
+            this.setState({
+                dataSource:data1
+            })
+        } else{
+            this.setState({
+                dataSource:data2
+            })
+        }
+        console.log(params)
+        console.log(key)
+
+
+
+        // this.setState({
+        //     loading: true
+        // });
+        // axios({
+        //     url: `${this.url.checkSite.page}`,
+        //     method: 'get',
+        //     headers: {
+        //         'Authorization': this.url.Authorization
+        //     },
+        //     params
+        // }).then(data => {
+        //     let res = data.data.data;
+        //     if(res && res.list) {
+        //         this.pagination.total = res['total'] ? res['total'] : 0;
+        //         for(let i = 0; i < res.list.length; i++) {
+        //             res['list'][i]['index'] = (res['page'] - 1) * 10 + i + 1;
+        //         }
+        //         this.setState({
+        //             dataSource: res.list
+        //         })
+        //     }
+        //     this.setState({
+        //         loading: false
+        //     })
+        // })
+    }
+
+
+    handleTableChange = (pagination) => {
+        this.pagination = pagination;
+        const tabKey = this.state.tabKey;
+        this.getTableParams(undefined,tabKey);
+    }
+
+    /**搜索事件*/
+    searchEvent = (searchContent) => {
+        this.setState({
+            searchContent
+        });
+        this.pagination.current = 1;
+        this.getTableParams(searchContent)
+    }
+
+    /**重置事件*/
+    reset = () => {
+        this.setState({
+            searchContent: undefined
+        });
+        this.pagination.current = 1;
+        const tabKey = this.state.tabKey;
+        this.getTableParams('',tabKey)
+    }
+
+    back = () => {
+        this.props.history.push('/repoBasic');
+    }
+
+}
+
+export default RepoRecordAccount;
